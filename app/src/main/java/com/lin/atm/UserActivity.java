a1a3a3d71141c1321d46204f159b8c43ce060d69
Andy0517 <ad870517@gmail.com>
2017-12-15 22:13:49 +0800

    Update UserActivity.java
    匿稱與電話資料以SharedPreferences方式儲存

--- app/src/main/java/com/lin/atm/UserActivity.java
+++ app/src/main/java/com/lin/atm/UserActivity.java
@@ -13,15 +13,25 @@ public class UserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
-    }
-    public void ok(View view){
         edname = (EditText) findViewById(R.id.ed＿name);
         edphone = (EditText) findViewById(R.id.ed_phone);
+        edName.setText(getSharedPreferences("atm", MODE_PRIVATE)
+                .getString("Name", ""));
+        edPhone.setText(getSharedPreferences("atm", MODE_PRIVATE)
+                .getString("Phone", ""));
+    }
+    public void ok(View view){
         String name = edname.getText().toString();
         String phone = edphone.getText().toString();
-        getIntent().putExtra("USER_NAME",name);
-        getIntent().putExtra("USER_PHONE",phone);
-        setResult(RESULT_OK,getIntent());
+        getIntent().putExtra("USER_NAME", name);
+        getIntent().putExtra("USER_PHONE", phone);
+        setResult(RESULT_OK, getIntent());
+        getSharedPreferences("atm", MODE_PRIVATE)
+                .edit()
+                .putString("NAME", name)
+                .putString("PHONE", phone)
+                .apply();
+        finish();
 
 
     }
